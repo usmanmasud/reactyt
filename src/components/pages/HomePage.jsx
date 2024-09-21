a8988a94aa5646cb72b7f66f4813a8e2ae1d6716
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-22 00:17:22 +0100

    useNavigate

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Homepage() {
+    const [username, setUsername] = useState('')
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate("/dashboard/profile", { state: { username } })
+    }
     return <div>This is a homepage
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        <button onClick={handleClick}>Go to Profile</button>
     </div>
 }
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,3 +1,7 @@
+import { useLocation } from "react-router-dom"
+
 export default function Profile() {
-    return <div>This is Profile</div>
+    const location = useLocation();
+    const { username } = location.state || {}
+    return <div>This is Profile for {username} </div>
 }
